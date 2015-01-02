19e9857aa01b205547a3d828be7a3c6204b997f1
Daniel Le Pelley <daniellepelley@gmail.com>
2015-01-02 09:48:46 +0000

    Add NeuralNet

---
+++ NeuralNetDistribution/NeuralNetDistribution/UnitTest1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AForge.Neuro;
+using AForge.Neuro.Learning;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NeuralNetDistribution
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double sigmoidAlphaValue = 3;
+
+            List<double[]> input = new List<double[]>();
+
+            input.Add(new double[] {0.2});
+            input.Add(new double[] {0.4});
+            input.Add(new double[] {0.6});
+            input.Add(new double[] {0.8});
+            input.Add(new double[] {1});
+
+            List<double[]> output = new List<double[]>();
+
+            output.Add(new double[] {0.1});
+            output.Add(new double[] {0.4});
+            output.Add(new double[] {0.6});
+            output.Add(new double[] {0.75});
+            output.Add(new double[] {0.81});
+
+            ActivationNetwork network = new ActivationNetwork(
+                new BipolarSigmoidFunction(sigmoidAlphaValue),
+                1, 1, 1);
+
+            BackPropagationLearning teacher = new BackPropagationLearning(network);
+
+            teacher.LearningRate = 0.1;
+            teacher.Momentum = 0;
+
+            while (true)
+            {
+                // run epoch of learning procedure
+                double error = teacher.RunEpoch(input.ToArray(), output.ToArray())/input.Count;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            List<double> inputs = new List<double>();
+            List<double> outputs = new List<double>();
+
+            var data = new double[] { 1,3.1,4.2478,5.7856,6.234,7.76,8.45,9.37,12,13,14,3.865,3.542,5.54,76,2.354,4.745,6,56,5.4,45,4.235,34,6.43,7.74,4.32 };
+
+            //var data = new double[] { 1,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5 };
+
+            //data = data.OrderBy(d => d).ToArray();
+
+            //ParetoShaped Data
+            //var data = new double[] { 10, 6.6, 5, 4, 3.3, 2.8, 2.5, 2.3, 2, 1.85 };
+
+            var mean = MathNet.Numerics.Statistics.ArrayStatistics.Mean(data);
+            var stDev = MathNet.Numerics.Statistics.ArrayStatistics.StandardDeviation(data);
+
+            //var distribution = new MathNet.Numerics.Distributions.Normal(mean, stDev);
+
+            var distribution = new MathNet.Numerics.Distributions.Normal(mean, stDev);
+
+
+            List<double> errorBefore = new List<double>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                double cumulativeDistribution = (double)(i + 1) / (double)data.Length;
+
+                inputs.Add(cumulativeDistribution);
+                
+                var actual = distribution.CumulativeDistribution(data[i]);
+
+                outputs.Add(actual);
+
+                var error = cumulativeDistribution / actual;
+                errorBefore.Add(error);
+            }
+
+            double averageErrorBefore = errorBefore.Average();
+
+            double baseLineAddition = MathNet.Numerics.Statistics.ArrayStatistics.Minimum(outputs.ToArray()) -
+                                      MathNet.Numerics.Statistics.ArrayStatistics.Minimum(inputs.ToArray());
+            double baseLineMultiple = 1 / (MathNet.Numerics.Statistics.ArrayStatistics.Maximum(outputs.ToArray()) - baseLineAddition);
+
+            double sigmoidAlphaValue = 1;
+
+            ActivationNetwork network = new ActivationNetwork(new SigmoidFunction(1),1, 4, 4, 1);
+                //new BipolarSigmoidFunction(sigmoidAlphaValue),
+                //1, 3, 1);
+
+            BackPropagationLearning teacher = new BackPropagationLearning(network);
+
+            teacher.LearningRate = 0.478587;
+            teacher.Momentum = 0.0;
+
+            double[][] nnInputs = inputs.Select(s => new double[] {s}).ToArray();
+            double[][] nnOutputs = outputs.Select(s => new double[] { (s - baseLineAddition) * baseLineMultiple}).ToArray();
+
+
+            for(int i = 0; i < 50000; i++)
+            {
+                // run epoch of learning procedure
+                double error = teacher.RunEpoch(
+                    nnInputs,
+                    nnOutputs
+                    ) / inputs.Count;
+            }
+
+            List<double> errors = new List<double>();
+            for(int i = 0; i < nnInputs.Length; i++)
+            {
+                double actual = network.Compute(nnInputs[i])[0];
+                double expected = nnOutputs[i][0];
+                errors.Add(Math.Abs(1 - (expected / actual)));
+            }
+
+            double nnAverageError = errors.Average();
+
+            errors = new List<double>();
+            List<double[]> actuals = new List<double[]>();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                double cumulativeDistribution = (double)(i + 1) / (double)data.Length;
+
+                double aiCumulativeDistribution = network.Compute(new double[] { cumulativeDistribution })[0];
+
+                aiCumulativeDistribution = (aiCumulativeDistribution/baseLineMultiple) + baseLineAddition;
+
+
+                var actual = distribution.InverseCumulativeDistribution(aiCumulativeDistribution);
+
+                errors.Add(Math.Abs(1 - (data[i] / actual)));
+                actuals.Add(new double[] { actual, data[i] });
+
+            }
+
+            double averageError = errors.Average();
+
+
+        }
+
+        [TestMethod]
+        public void ParetoTest1()
+        {
+            var dist = new MathNet.Numerics.Distributions.Pareto(1, .45);
+            List<double> results = new List<double>();
+            
+            for (double d = 1; d <= 10.0; d++)
+            {
+                results.Add(50000 - (50000 * dist.CumulativeDistribution(d)));
+            }
+
+            var result = string.Join(",", results);
+
+        }
+
+        [TestMethod]
+        public void CurtainsLeadTime()
+        {
+            var mean = 17.54F;
+            var std = 15.16F;
+
+            var dist = new MathNet.Numerics.Distributions.Normal(mean, std);
+
+            List<double> results = new List<double>();
+            for (double d = 0; d <= 1.0; d+=0.05)
+            {
+                results.Add(dist.InverseCumulativeDistribution(d));
+            }
+
+
+        }
+
+        [TestMethod]
+        public void Erlang()
+        {
+            var mean = 17.54F;
+            var std = 15.16F;
+
+            var dist = new MathNet.Numerics.Distributions.Erlang(5, std);
+
+            List<double> results = new List<double>();
+            for (double d = 0; d <= 1.0; d += 0.05)
+            {
+                //results.Add(dist(d));
+            }
+
+
+        }
+    }
+}
---
+++ WpfApplicationRXPrototyping/WpfApplicationRXPrototyping/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Reactive;
+using System.Reactive.Linq;
+//using System.Reactive.Concurrency;
+using System.Windows.Threading;
+using System.Collections.ObjectModel;
+using Moq;
+using Moq.Language.Flow;
+
+namespace WpfApplicationRXPrototyping
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private Random random = new Random(DateTime.Now.Millisecond);
+
+        private bool buttonEnabled = true;
+
+        private readonly IObservable<string> observablePropertyChanges; 
+
+        private ObservableCollection<string> collection = new ObservableCollection<string>();
+
+        private IObservableRepository<int> observableRepository;
+
+        public ObservableCollection<string> Collection
+        {
+            get { return collection; }
+            set { collection = value; }
+        }
+
+        public bool ButtonEnabled
+        {
+            get
+            {
+                return buttonEnabled;
+            }
+            set
+            {
+                buttonEnabled = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("ButtonEnabled"));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            observablePropertyChanges = this.ObservablePropertyChanges();
+            
+            observableRepository = new ObservableRepository<int>(new TestRepository());
+            observableRepository.GetAll().Subscribe((i) => collection.Add(i.ToString()));
+
+            observableRepository.SaveAsync(3);
+
+            this.DataContext = this;
+        }
+
+        private string GetString(int time)
+        {
+            System.Threading.Thread.Sleep(time);
+            return string.Format("{0} {1}", time, System.Threading.Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var ob1 = observablePropertyChanges
+                .Where(p => p == "ButtonEnabled" && this.ButtonEnabled == false)
+                .Timeout(new TimeSpan(0, 0, 3))
+                .Subscribe(_ => { }, ex => ButtonEnabled = true);
+
+            ButtonEnabled = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public static class Extensions
+    {
+        public static IObservable<string> ObservablePropertyChanges(this INotifyPropertyChanged source)
+        {
+            return Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                handler => (sender1, args) => handler(sender1, args),
+                x => source.PropertyChanged += x,
+                x => source.PropertyChanged -= x)
+                .Select(event1 =>
+                    event1.EventArgs.PropertyName);
+        }
+
+
+        public static IDisposable SubscribeObservableCollection<T>(this IObservable<T> source,
+                                                                   ObservableCollection<T> collection)
+        {
+            return source.SubscribeObservableCollection(collection, DispatcherPriority.Normal);
+        }
+
+        public static IDisposable SubscribeObservableCollection<T>(this IObservable<T> source,
+                                                                   ObservableCollection<T> collection,
+                                                                   DispatcherPriority dispatcherPriority)
+        {
+            return source.ObserveOnDispatcher(dispatcherPriority)
+                         .Subscribe(s => collection.Add(s));
+        }
+    }
+
+    public class TestRepository : IRepository<int>
+    {
+        public IEnumerable<int> GetAll()
+        {
+
+
+
+            return Enumerable.Range(1, 200);
+        }
+    }
+
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+    }
+
+    public interface IObservableRepository<T>
+    {
+        IObservable<T> GetAll();
+        Task SaveAsync(T entity);
+    }
+
+    public class ObservableRepository<T> : IObservableRepository<T>
+    {
+        private IRepository<T> repository;
+
+        private IObservable<T> observable;
+
+        public ObservableRepository(IRepository<T> repository)
+        {
+            this.repository = repository;
+            observable = repository.GetAll().ToObservable();
+        }
+
+        public IObservable<T> GetAll()
+        {
+            return observable;
+        }
+
+        public async Task SaveAsync(T entity)
+        {
+            //Task.Delay() to simulate some load
+            await Task.Delay(4000);
+        }
+    }
+
+}
